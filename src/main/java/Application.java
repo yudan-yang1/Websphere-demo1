a0f23c7a00c50e74250792d580b4af558f8efe29
yudan-yang1 <yudan.yang1@thoughtworks.com>
2023-02-22 09:57:29 +0800

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+import modules.HelloWorld;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+  public static void main(String[] args) {
+    ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+    obj.getMessage();
+  }
+}
